327514d76cfa943cf67adfce482242f47826ad6b
pritam sarkar <pritam7796419792@gmail.com>
2024-07-23 23:31:14 +0530

    theme and all the froentend are set now to push the code in thr git hub

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { IoHome } from "react-icons/io5";
 import { IoIosAdd } from "react-icons/io";
 import { IoMdNotifications } from "react-icons/io";
 import { CiSquareQuestion } from "react-icons/ci";
 import { MdSupportAgent } from "react-icons/md";
+import ContextMaker from "../../context/ContextMaker";
 
 function Footer() {
+  const { theme } = useContext(ContextMaker);
   return (
-    <div className="w-full h-14 bg-black text-3xl flex justify-between px-4 items-center">
+    <div
+      className={`w-full h-14 ${
+        theme ? "bg-black" : "bg-white"
+      } text-3xl flex justify-between bg-opacity-60 backdrop-blur-sm  px-4 items-center rounded-t-xl`}
+    >
       <IoHome />
       <MdSupportAgent />
-      <span className="text-6xl">
+      <span className="text-6xl ">
         <IoIosAdd />
       </span>
       <CiSquareQuestion />
